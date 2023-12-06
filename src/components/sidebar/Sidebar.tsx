cd990470d59693ce7108f2bfa1b0f1716b5e8f8f
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-06 11:48:51 +0900

    add

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -5,7 +5,9 @@ const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className='sidebarRight'>
-        
+        <div className='sidebarTop'>
+          <h3>sidebar</h3>
+        </div>
       </div>
     </div>
   )
