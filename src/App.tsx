fb68ccd6c1f3aebd942d09799a5f5c4a333e1557
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-06 11:02:03 +0900

    サイドバーコンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      discord
+      <Sidebar />
     </div>
   );
 }
