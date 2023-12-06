a5bff3ef3bdba55e831f902b92c84c7543fd9167
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-06 11:02:19 +0900

    add

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className='sidebar'>
+      <div className='sidebarRight'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
