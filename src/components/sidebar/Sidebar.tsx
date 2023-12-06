e878d8331f2c35b14a41080bb5d6198da8553714
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-06 15:27:23 +0900

    デザイン

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import './Sidebar.scss'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className='sidebarRight'>
         <div className='sidebarTop'>
-          <h3>sidebar</h3>
+          <h3>Menu</h3>
           <ExpandMoreIcon />
+        </div>
+        <div className='sidebarChannel'>
+          <div className='sidebarChannelHeader'>
+            <div className='sidebarHeader'>
+              <h4>トークチャンネル</h4>
+            </div>
+            <AddCircleIcon className='sidebarIcon' />
+          </div>
         </div>
       </div>
     </div>
