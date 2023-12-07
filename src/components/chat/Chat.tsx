c890e4c476a61449df6f77585492027e44d820a3
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-07 18:28:48 +0900

    add

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import './Chat.scss'
 import ChatHeader from './ChatHeader'
+import ChatMessage from './ChatMessage'
 
 const Chat = () => {
   return (
     <div className='chat'>
       <ChatHeader />
-      <div className='chatMessage'></div>
-      <div className='chatInput'></div>
+      <div className='chatMessage'>
+        <ChatMessage />
+      </div>
+      <div className='chatInput'>
+        <form>
+          <input type='text' placeholder='メッセージを送信'></input>
+          <button type='submit' className='chatInputButton'>
+            送信
+          </button>
+        </form>
+      </div>
     </div>
   )
 }
