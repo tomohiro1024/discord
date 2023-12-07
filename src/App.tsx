64fa8bbb94dab2fc5cbee8ca9fd5231ad1d8e9ca
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-07 12:01:19 +0900

    add chat Componet

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
 import Sidebar from './components/sidebar/Sidebar';
+import Chat from './components/chat/Chat';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Chat />
     </div>
   );
 }
---
+++ src/components/chat/Chat.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Chat.scss'
+
+const Chat = () => {
+  return (
+    <div className='chat'>
+      Chat
+    </div>
+  )
+}
+
+export default Chat
