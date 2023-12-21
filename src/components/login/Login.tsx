33654b94d3899a4b3152a2478c32b57791a7981b
tomohiro1024 <gongyezhihong732@gmail.com>
2023-12-21 15:27:23 +0900

    signInWithPopup in Firebase

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import "./Login.scss"
 import { Button } from '@mui/material'
+import { signInWithPopup } from 'firebase/auth'
+import { auth, provider } from '../../firebase'
 
 const Login = () => {
+
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch((err) => {
+      alert(err.message)
+    })
+  }
+
   return (
     <div className='login'>
       <div className='loginForm'>
       </div>
-      <Button>ログイン</Button>
+      <Button onClick={signIn}>ログイン</Button>
     </div>
   )
 }
